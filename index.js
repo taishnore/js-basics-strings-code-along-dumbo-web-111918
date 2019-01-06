9724d5de8eb321eb5b23ad743186d35079560aa0
Taimur Shah <tshah@scu.edu>
2019-01-06 02:00:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const currentUser = "Taimur Shah";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
@@ -5,3 +6,5 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+=======
+>>>>>>> 48727913f1949f3f2e933b6431e468d80de4c14d
